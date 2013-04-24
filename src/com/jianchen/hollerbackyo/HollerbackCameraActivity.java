608947294c2691816e5b910cc72f2e3bbd7709f9
Jian Chen <quantumsix492@gmail.com>
2013-04-24 11:22:27 -0700

    moving file access methods into util class

--- src/com/jianchen/hollerbackyo/HollerbackCameraActivity.java
+++ src/com/jianchen/hollerbackyo/HollerbackCameraActivity.java
@@ -7,6 +7,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import com.jianchen.hollerbackyo.util.FileUtil;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.graphics.Bitmap;
@@ -206,7 +208,8 @@ public class HollerbackCameraActivity extends Activity implements
 		recorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_480P));
 
 		// Step 4: Set output file
-		recorder.setOutputFile(getOutputMediaFile(MEDIA_TYPE_VIDEO).toString());
+		recorder.setOutputFile(FileUtil.getOutputMediaFile(
+				FileUtil.MEDIA_TYPE_VIDEO).toString());
 
 		// Step 5: Set the preview output
 		recorder.setPreviewDisplay(preview.getHolder().getSurface());
@@ -380,50 +383,4 @@ public class HollerbackCameraActivity extends Activity implements
 	public void onClick(View v) {
 
 	}
-
-	public static final int MEDIA_TYPE_IMAGE = 1;
-	public static final int MEDIA_TYPE_VIDEO = 2;
-
-	/** Create a file Uri for saving an image or video */
-	private static Uri getOutputMediaFileUri(int type) {
-		return Uri.fromFile(getOutputMediaFile(type));
-	}
-
-	/** Create a File for saving an image or video */
-	private static File getOutputMediaFile(int type) {
-		// To be safe, you should check that the SDCard is mounted
-		// using Environment.getExternalStorageState() before doing this.
-
-		File mediaStorageDir = new File(
-				Environment
-						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-				"MyCameraApp");
-		// This location works best if you want the created images to be shared
-		// between applications and persist after your app has been uninstalled.
-
-		// Create the storage directory if it does not exist
-		if (!mediaStorageDir.exists()) {
-			if (!mediaStorageDir.mkdirs()) {
-				Log.d("MyCameraApp", "failed to create directory");
-				return null;
-			}
-		}
-
-		// Create a media file name
-		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
-				.format(new Date());
-		File mediaFile;
-		if (type == MEDIA_TYPE_IMAGE) {
-			mediaFile = new File(mediaStorageDir.getPath() + File.separator
-					+ "IMG_" + timeStamp + ".jpg");
-		} else if (type == MEDIA_TYPE_VIDEO) {
-			mediaFile = new File(mediaStorageDir.getPath() + File.separator
-					+ "VID_" + timeStamp + ".mp4");
-		} else {
-			return null;
-		}
-
-		return mediaFile;
-	}
-
 }
