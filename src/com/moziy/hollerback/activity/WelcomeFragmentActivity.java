e2ac8eb2f21b7eec294ae1434296ddbf76480e08
Jian Chen <quantumsix492@gmail.com>
2013-05-06 11:39:55 -0700

    adding the login/signup screen

--- src/com/moziy/hollerback/activity/WelcomeFragmentActivity.java
+++ src/com/moziy/hollerback/activity/WelcomeFragmentActivity.java
@@ -1,8 +1,11 @@
 package com.moziy.hollerback.activity;
 
-import com.moziy.hollerback.R;
-
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.moziy.hollerback.R;
+import com.moziy.hollerback.fragment.WelcomeFragment;
 
 public class WelcomeFragmentActivity extends HollerbackBaseFragmentActivity {
 
@@ -17,12 +20,23 @@ public class WelcomeFragmentActivity extends HollerbackBaseFragmentActivity {
 		// TODO Auto-generated method stub
 		super.onCreate(bundle);
 		setContentView(R.layout.welcome_fragment_activity);
+		initFragment();
 	}
 
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
+	}
+
+	// TODO Abstract to fragment manager
+	public void initFragment() {
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager
+				.beginTransaction();
+		WelcomeFragment fragment = new WelcomeFragment();
+		fragmentTransaction.add(R.id.fragment_holder, fragment);
+		fragmentTransaction.commit();
 	}
 
 }
--- src/com/moziy/hollerback/fragment/WelcomeFragment.java
+++ src/com/moziy/hollerback/fragment/WelcomeFragment.java
@@ -1,13 +1,49 @@
 package com.moziy.hollerback.fragment;
 
+import com.moziy.hollerback.R;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class WelcomeFragment extends BaseFragment {
 
 	@Override
-	protected void initializeView(View view) {
-		// TODO Auto-generated method stub
-		
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View fragmentView = inflater.inflate(R.layout.welcome_fragment, null);
+
+		initializeView(fragmentView);
+		return fragmentView;
 	}
 
+	@Override
+	protected void initializeView(View view) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	public static WelcomeFragment newInstance(int num) {
+
+		WelcomeFragment f = new WelcomeFragment();
+
+		// Supply num input as an argument.
+		Bundle args = new Bundle();
+		args.putInt("num", num);
+		f.setArguments(args);
+		return f;
+	}
 }
